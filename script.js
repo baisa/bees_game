b5cb315d6ed898f26ddda1e6e2a6ce5d7b7f3775
Barbara Slawinska <slawinskabarbara@gmail.com>
2016-02-25 13:34:45 +0100

    random value from array

--- script.js
+++ script.js
@@ -10,3 +10,21 @@ var session = bees[Math.floor(Math.random()*bees.length)];
 console.log(session);
 
 console.log("How many bees do you see?")
+
+function myFunction() {
+    var x = document.getElementById("fname").value;
+    var n = Number(x);
+    if (n == session.length) {
+    answer ="Correct!";
+    } else {
+    answer = "Try again!";
+    };
+    document.getElementById("demo").innerHTML = answer;
+}
+
+function imageFunction() {
+    var x = document.images.length;
+    document.getElementById("demodemo").innerHTML = x;
+}
+
+
