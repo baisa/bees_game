1a7beddb537b37e107cac47dfba3e371d6fc8042
Barbara Slawinska <slawinskabarbara@gmail.com>
2016-03-03 14:13:53 +0100

    checkFunction implementation

--- script.js
+++ script.js
@@ -1,32 +1,56 @@
-var number = (Math.floor(Math.random() * (11 - 1)) + 1);
-var array = [];
+var array, number;
 
-function newImage() {
-  var image = new Image();
-  image.src='http://www.clker.com/cliparts/V/y/D/s/5/v/bee-hi.png';
-  document.getElementById("drawing").appendChild(image);
-}
+function setup() {
+  document.getElementById("drawing").innerHTML = null;
+  number = (Math.floor(Math.random() * (11 - 1)) + 1);
+  array = [];
 
-for (i = 0; i < number; i++) { 
-    var image = newImage();
-    array.push(image);
-}
+  function newImage() {
+    var image = new Image();
+    image.src='http://www.clker.com/cliparts/V/y/D/s/5/v/bee-hi.png';
+    document.getElementById("drawing").appendChild(image);
+  };
 
-
-function myFunction() {
-    var x = document.getElementById("fname").value;
-    var n = Number(x);
-    if (n == array.length) {
-    answer ="Correct!";
-    } else {
-    answer = "Try again!";
+    for (i = 0; i < number; i++) { 
+      var image = newImage();
+      array.push(image);
     };
-    document.getElementById("demo").innerHTML = answer;
-}
 
-function imageFunction() {
-    var x = document.images.length;
-    document.getElementById("demodemo").innerHTML = x;
-}
+};
+
+setup();
+
+  function myFunction() {
+      var x = document.getElementById("fname").value;
+      var n = Number(x);
+      if (n == array.length) {
+        answer ="Correct!";
+      } else {
+        answer = "Try again!";
+      }
+      document.getElementById("demo").innerHTML = answer;
+  };
+
+  function checkFunction() {
+    var elements = document.getElementsByClassName("given_answer");
+    for(var i = 0, length = elements.length; i < length; i++) {
+      var a = elements[i].value;
+
+  
+      var b = Number(a);
+      if (b == array.length) {
+      var sss = "sss";
+      
+      };
+    }
+    document.getElementById("demo1").innerHTML = sss;
+  };
+
+
+
+
+
+
+
 
 
